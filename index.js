f57dc191c739348aee15a6893699411fc81bd01c
Samuel Fernandes <samukabiska@gmail.com>
2025-04-07 20:37:07 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+alert ("Primeiro Contato com JavaScript!");
+alert (5+5);
+
+document.getElementById("texto").innerHTML="Escrevendo Usando <b>JavaScript!</b>";
+
+// Document.getElementById(texto): Ele esta capturando o comando inserid//
+// innerHTML: Interação com a nossa pagina//
